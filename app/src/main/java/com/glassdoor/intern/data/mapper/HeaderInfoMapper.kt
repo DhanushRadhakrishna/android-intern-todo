642107381c0e3e0e4da23da1441ba139fee8f0bb
DhanushRadhakrishna <dradh003@ucr.edu>
2025-02-08 17:36:54 -0800

    completed HeadInfo TODOs in data and domain packages

--- app/src/main/java/com/glassdoor/intern/data/mapper/HeaderInfoMapper.kt
+++ app/src/main/java/com/glassdoor/intern/data/mapper/HeaderInfoMapper.kt
@@ -19,10 +19,13 @@ import javax.inject.Inject
 internal class HeaderInfoMapper @Inject constructor() {
 
     /**
-     * TODO: Complete the transformation logic
+     *DONE TODO: Complete the transformation logic
      */
     fun toDomain(header: HeaderInfoDto, items: List<ItemInfoDto>): HeaderInfo = with(header) {
         HeaderInfo(
+            title = title,
+            description = description,
+            timestamp = timestamp,
             items = items.map(::toDomain)
         )
     }
